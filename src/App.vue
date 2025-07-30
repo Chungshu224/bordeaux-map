f580d2e93a51604a2018c4bbe0237ff084573284
Chungshu224 <chungshu224@gmail.com>
2025-07-31 01:13:27 +0800

    feat: 移除不必要的 logo CSS 樣式，簡化樣式表

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,5 @@ import BordeauxMap from './components/BordeauxMap.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+/* 如無 logo，可移除下方 CSS */
 </style>
